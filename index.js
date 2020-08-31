ab2b81d4183cf00fa75fe2d7d93d6b34456cd81c
David Krmpotic <david.krmpotic@gmail.com>
2020-08-31 16:19:19 +0200

    provisional swarm tags inquiry

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import fs from 'fs';
 import request from 'request';
 
+import tagStatus from './lib/tagStatus';
+
 const url = 'http://localhost:8888/files';
 
 const req = request.post(url, (err, res, body) => {
@@ -8,6 +10,14 @@ const req = request.post(url, (err, res, body) => {
     console.log('Error');
   } else {
     console.log(body);
+
+    const tagId = res.headers['swarm-tag-uid'];
+
+    setInterval(() => {
+      tagStatus(tagId)
+        .then(console.log)
+        .catch(e => `Error: ${e}`);
+    }, 1000);
   }
 });
 
